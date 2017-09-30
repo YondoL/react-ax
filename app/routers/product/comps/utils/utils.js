396f93123d03cb8fb4940573e1a6f3e443848914
1628916868@qq.com <1628916868@qq.com>
2017-09-30 15:09:25 +0800

    添加截取url字符串方法

--- app/routers/product/comps/utils/utils.js
+++ app/routers/product/comps/utils/utils.js
@@ -4,5 +4,12 @@ module.exports = {
     str.splice(3, 4, '*', '*', '*', '*')
     str = str.join('')
     return str
+  },
+  queryString: (name) => {
+    let result = location.search.match(new RegExp('[\?\&]' + name + '=([^\&]+)','i'))
+    if (result === null || result.lenght < 1) {
+      return ''
+    }
+    return result[1]
   }
 }
